18556e1fb6a95962a352e12e110d01ba13cbe70a
Takanobu Morishita <morishita@keio.jp>
2022-11-04 12:01:58 +0900

    feature: valid

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,5 @@
 class DocumentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @documents = Document.all
   end
@@ -14,18 +15,27 @@ class DocumentsController < ApplicationController
   def create
     @document = Document.new(document_params)
     @document.user_id = current_user.id
-    @document.save
-    redirect_to document_path(@document)
+    if @document.save
+    redirect_to document_path(@document), notice: '投稿に成功しました。'
+    else
+      render :new
+    end
   end
 
   def edit
     @document = Document.find(params[:id])
+    if @document.user != current_user
+      redirect_to documents_path, alert: '不正なアクセスです'
+    end
   end
 
   def update
     @document = Document.find(params[:id])
-    @document.update(document_params)
-    redirect_to document_path(@document)
+    if @document.update(document_params)
+      redirect_to document_path(@document), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
   end
 
   private
